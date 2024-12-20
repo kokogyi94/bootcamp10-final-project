2707e43c41e35105f0844d9867066234efa8ff6e
Ko Ko Gyi <kokogyikkg170@gmail.com>
2024-12-20 11:30:12 +0630

    init project

---
+++ test-spring-boot-demo/src/main/java/com/jdc/TestSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringBootDemoApplication.class, args);
+	}
+
+}
