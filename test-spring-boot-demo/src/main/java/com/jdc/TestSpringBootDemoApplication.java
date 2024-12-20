85309b1e6ee097d141f5f6de5a5a6481d83b30a0
Ko Ko Gyi <kokogyikkg170@gmail.com>
2024-12-20 11:58:43 +0630

    update

--- test-spring-boot-demo/src/main/java/com/jdc/TestSpringBootDemoApplication.java
+++ test-spring-boot-demo/src/main/java/com/jdc/TestSpringBootDemoApplication.java
@@ -8,6 +8,8 @@ public class TestSpringBootDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestSpringBootDemoApplication.class, args);
+		
+		System.out.println("hello");
 	}
 
 }
